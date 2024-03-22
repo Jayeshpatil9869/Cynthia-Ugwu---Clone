cffd82b964d87b6d8b2230f1f1b8fa8b6e6ce034
Jayesh Patil <97843592+Jayeshpatil9869@users.noreply.github.com>
2024-03-22 21:57:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true,
+});
+
+function preLoader() {
+  document.addEventListener("DOMContentLoaded", function () {
+    // Remove 'loading' class from body to hide preloader
+    document.body.classList.remove("loading");
+
+    // Optionally, you can add a delay before hiding the preloader
+    setTimeout(function () {
+      document.body.classList.add("loaded");
+    }, 1000); // Adjust the delay time (in milliseconds) as needed
+  });
+}
+
+function firstPageAnimation() {
+  var tl = gsap.timeline();
+
+  tl.from(".nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingelm", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      stagger: 0.2,
+      delay: -1,
+    })
+    .from(".bottomHeading", {
+      y: "-10",
+      opacity: 0,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
+}
+
+var timeout;
+
+function circleChaptaKaro() {
+  // define default scale value
+  var xscale = 1;
+  var yscale = 1;
+
+  var xprev = 0;
+  var yprev = 0;
+
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeout);
+
+    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
+
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+
+    circleMouseFollower(xscale, yscale);
+
+    timeout = setTimeout(function () {
+      document.querySelector(
+        ".minicircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+    }, 100);
+  });
+}
+
+function circleMouseFollower(xscale, yscale) {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector(
+      ".minicircle"
+    ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`;
+  });
+}
+
+circleChaptaKaro();
+circleMouseFollower();
+
+preLoader();
+firstPageAnimation();
+
+
+
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+  var rotate = 0;
+  var diffrot = 0;
+
+  elem.addEventListener("mouseleave", function (dets) {
+    gsap.to(elem.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5,
+    });
+  });
+
+  elem.addEventListener("mousemove", function (dets) {
+    var diff = dets.clientY - elem.getBoundingClientRect().top;
+    diffrot = dets.clientX - rotate;
+    rotate = dets.clientX;
+    gsap.to(elem.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: dets.clientX,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+    });
+  });
+});
